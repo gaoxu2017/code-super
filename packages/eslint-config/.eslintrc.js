5a929862d3c5c449db0530ef87b4293475d4d9b1
gaoxu <Gaoxu13167316938@163.com>
2024-07-25 22:20:32 +0800

    chore: style 修改

--- packages/eslint-config/.eslintrc.js
+++ packages/eslint-config/.eslintrc.js
@@ -1,13 +1,3 @@
-/*
- * @Author: gaoxu Gaoxu13167316938@163.com
- * @Date: 2024-07-23 22:29:57
- * @LastEditors: gaoxu Gaoxu13167316938@163.com
- * @LastEditTime: 2024-07-23 22:30:51
- * @FilePath: \engineeringRules\packages\eslint-config\eslint.config.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 module.exports = {
-  extends:[
-    './index'
-  ].map(require.resolve)
-}
+  extends: ['./index'].map(require.resolve),
+};
