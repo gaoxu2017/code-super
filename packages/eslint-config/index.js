28ded8ec7ea1241338ac4bfb09e1205dec5179ad
gaoxu <Gaoxu13167316938@163.com>
2024-07-25 21:00:00 +0800

    chore: style modify

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,11 +1,3 @@
-/*
- * @Author: gaoxu Gaoxu13167316938@163.com
- * @Date: 2024-07-23 22:51:34
- * @LastEditors: gaoxu Gaoxu13167316938@163.com
- * @LastEditTime: 2024-07-25 16:44:52
- * @FilePath: \engineeringRules\packages\eslint-config\index.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 module.exports = {
   extends: [
     './rules/base/best-practices.js',
